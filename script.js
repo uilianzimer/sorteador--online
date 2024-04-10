d8807f891aa617bbe4a155f3e23730d691e8d717
uilianzimer <uilianzimer333@gmail.com>
2024-04-10 19:47:54 -0300

    sorteador-online

---
+++ script.js
@@ -0,0 +1,16 @@
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+
+    if (min >= max){
+        alert("O valor minimo tem que ser MENOR que o valor máximo")
+    }else{
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        
+        alert(result);
+    }
+
+    }
+    
+    
+
